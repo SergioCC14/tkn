94eaae2c899fc26048fc407ef98ca155d47fe482
Sergio Canis <sergio.canis@outlook.com>
2017-01-28 01:41:43 +0100

    add new keynote: Codenares Alcala 2017

---
+++ keynotes/codenares-alcala-2017.rb
@@ -0,0 +1,119 @@
+# encoding: utf-8
+
+center <<-EOS
+  \e[1mCodenares\e[0m
+
+  #codenaresADH
+
+EOS
+
+block <<-EOS
+
+\e[1mJose Dongil (@jdonsan)\e[0m
+Eterno Junior. Todero. Vengo a aprender y no a entorpecer.
+
+\e[1mSergio Canis (@sergcns)\e[0m
+Internet estaba allí cuando llegué.
+
+EOS
+
+section "Origen del Codenares" do
+  block <<-EOS
+
+  \e[1m14 de Noviembre\e[0m
+  2 años después del último Betabeers Guadalajara...
+
+  \e[1m+-16 de Noviembre\e[0m
+  Lanzamos una encuesta de sondeo.
+
+  \e[1m29 de Noviembre\e[0m
+  José y Sergio quedan a solas en una cafetería.
+  Surge el amor.
+
+  EOS
+
+  block <<-EOS
+
+  \e[1m1 de Diciembre\e[0m
+  Lanzamos Meetup
+
+  \e[1m28 de Enero\e[0m
+  Codenares Alcalá de Henares
+
+  \e[1mXX de Marzo/Abril\e[0m
+  Codenares XXXjara
+
+  \e[1m14 de Diciembre\e[0m
+  Nos compra UBER.
+
+  EOS
+end
+
+section "Objetivo del Codenares" do
+  code <<-EOS
+    * Dar valor al Corredor del Henares
+
+    * Aprender
+
+    * Divertirnos
+  EOS
+end
+
+section "Planes de futuro" do
+  code <<-EOS
+    Yo k se tio xd xd
+  EOS
+
+  code <<-EOS
+    1. Ciudades del Corredor del Henares.
+    2. Ponentes locales (con invitados estrella).
+    3. No streaming.
+    4. Comunidad libre.
+  EOS
+
+end
+
+section "Agradecimientos" do
+  block <<-EOS
+    \e[1m Victor J Pascual\e[0m
+  EOS
+
+  center <<-EOS
+    \e[1m Julio Berzal\e[0m
+      Agua de Mayo
+  EOS
+
+  center <<-EOS
+    \e[1m Rubén Aguilera\e[0m
+    &
+    \e[1m Angel J. Alvarez Miguel\e[0m
+  EOS
+
+  block <<-EOS
+    \e[1m
+       ─────      ───
+      │    ╱     │   │
+      │   │      │   │
+      │   ╲     ╱   ╱
+      ╲    ────    ╱
+       ╲          ╱
+         ────────
+          Beperk\e[0m
+
+    Luis Antonio Carrasco
+    Santi Ruiz
+  EOS
+
+  block <<-EOS
+    \e[1m A todos vosotros, titanes.\e[0m
+  EOS
+
+section "One more thing" do
+  block <<-EOS
+    Enjoy! :-)
+  EOS
+end
+
+end
+
+__END__
