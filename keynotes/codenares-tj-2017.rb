3e4274baa4aa9c37fc4304aacb7106aceace14e5
Sergio Canis <sergio.canis@outlook.com>
2017-06-10 01:16:30 +0200

    add new keynote: Codenares Torrejon 2017

---
+++ keynotes/codenares-tj-2017.rb
@@ -0,0 +1,86 @@
+# encoding: utf-8
+
+center <<-EOS
+  \e[1mIII Codenares\e[0m
+
+EOS
+
+block <<-EOS
+
+\e[1mInformamos en\e[0m
+twitter.com/codenares
+
+
+\e[1mComunicamos en\e[0m
+meetup.com/codenares
+
+
+\e[1mCompartimos\e[0m
+medium.com/codenares
+
+\e[1mPostureamos con\e[0m
+#somosCodenares
+
+
+EOS
+
+section "Hagamos Recap" do
+  block <<-EOS
+
+┌─────────────┬─────────────────────┬───────────────┬────────────────────┐
+│  \e[1mSede\e[0m       │  Alcalá de henares  │  Guadalajara  │  Torrejón de Ardoz │
+├─────────────┼─────────────────────┼───────────────┼────────────────────┤
+│  \e[1mFecha\e[0m      │      28 de enero    │   8 de abril  │     10 de Junio    │
+├─────────────┼─────────────────────┼───────────────┼────────────────────┤
+│  \e[1mCharlas\e[0m    │          2          │       2       │         2          │
+├─────────────┼─────────────────────┼───────────────┼────────────────────┤
+│  \e[1mAsistentes\e[0m │          38         │       45      │         55         │
+├─────────────┼─────────────────────┼───────────────┼────────────────────┤
+│  \e[1mComunidad\e[0m  │          123        │       159     │         224        │
+└─────────────┴─────────────────────┴───────────────┴────────────────────┘
+
+
+  EOS
+end
+
+section "Pilares de Codenares" do
+  center <<-EOS
+      \e[1mDiversidad\e[0m
+  EOS
+
+  center <<-EOS
+      \e[1mAmor por la tecnología\e[0m
+  EOS
+
+  center <<-EOS
+      \e[1mADN Henares\e[0m
+  EOS
+
+end
+
+section "Actividades paralelas" do
+  code <<-EOS
+  * Blog en Medium
+
+  * ¿Youtube?
+
+  * ¿Patrocinadores?
+
+  EOS
+
+section "One more thing" do
+  center <<-EOS
+    Because the people who are crazy enough
+      to think they can \e[1mchange the world\e[0m,
+      are the ones who do.
+  EOS
+
+  center <<-EOS
+    Enjoy! :-)
+  EOS
+
+end
+
+end
+
+__END__
